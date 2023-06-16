f002d9d153258619d18b319f57006dc589b50186
Jesse Veldmaat <J.Veldmaat@student.han.nl>
2023-06-16 14:54:35 +0200

    inital setup

---
+++ src/main/java/nl/han/dea/onderzoek/spotitubespringboot/SpotitubeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package nl.han.dea.onderzoek.spotitubespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotitubeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotitubeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/han/dea/onderzoek/spotitubespringboot/SpotitubeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.han.dea.onderzoek.spotitubespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotitubeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
